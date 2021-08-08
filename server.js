d93b902e66f15360b69468e3d9adce99e009e0ea
Brian <blsvsanteliz@gmail.com>
2021-08-08 19:00:27 -0400

    Create functionality to send and recived message of cliente using socket

--- server.js
+++ server.js
@@ -1,23 +1,60 @@
 const { Server } = require('net');
 const HOST = '0.0.0.0';
 
+const EXIT_SOCKET = 'exit';
+const clientConnected = new Map();
+
 const error = (msg) => {
   console.log(msg);
   process.exit(1);
 };
 
+const sendMessage = (msg, socketSend) => {
+  Array.from(clientConnected.keys())
+    .filter((socketCliente) => socketCliente !== socketSend)
+    .forEach((socketCliente) => {
+      socketCliente.write(msg);
+    });
+};
 const initSocket = (port) => {
   const server = new Server();
 
   server.on('connection', (socket) => {
-    socket.on('close', () => {
-      console.log(`Disconected: ${socket}`);
+    socket.setEncoding('utf-8');
+    console.log(
+      `Client connected: ${socket.remoteAddress}:${socket.remotePort}`
+      );
+    socket.on('data', (data) => {
+      const client = clientConnected.has(socket);
+      if (!client) {
+        clientConnected.set(socket, data);
+        console.log(
+          `¡Client with username ${data} has been register to chat successfully you Adress is: ${socket.remoteAddress}:${socket.remotePort}!`
+        );
+        return;
+      }
+      if (data === EXIT_SOCKET) {
+        clientConnected.delete(data);
+        socket.end();
+        return;
+      }
+      const usernameCliente = clientConnected.get(socket)
+      const msgToSendClient = `${usernameCliente} Send: ${data}`;
+      console.log(`Client ${usernameCliente}: ${data}`);
+      sendMessage(msgToSendClient, socket);
     });
+
+    socket.on('close', () => {
+      console.log(
+        `Client Disconected with adress: ${socket.remoteAddress}:${socket.remotePort}`
+      );
+    });
+    socket.on('error', (e) => error(e.message));
   });
+
   server.listen({ port, host: HOST }, () => {
     console.log(`Listening on port ${port} and ${HOST}`);
   });
-
   server.on('error', (err) => error(err.message));
 };
 
