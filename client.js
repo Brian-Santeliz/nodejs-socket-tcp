099716ed14b2229c03c6f21cf07e100bde4dc091
Brian <blsvsanteliz@gmail.com>
2021-08-08 19:00:54 -0400

    Fisnish setup of cliente, working to send message and register client

--- client.js
+++ client.js
@@ -1,24 +1,47 @@
 const { Socket } = require('net');
+const EXIT_SOCKET = 'exit';
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const error = (msg) => {
   console.log(msg);
   process.exit(1);
 };
 
 const connectToSocket = (host, port) => {
-  console.log(`Conecting to server ${host}:${port}`);
-
+  console.log(`Conecting to server ${host}:${port}...`);
   const socket = new Socket();
-  // Setting of socket
-  socket.setDefaultEncoding('utf-8');
+  
   socket.connect({
     host,
     port,
   });
-  socket.on("connect", ()=>{
+  socket.on('connect', () => {
+    socket.setEncoding('utf-8');
     console.log('Connected successfully!');
+    // Set username of cliente and send to server
+    readline.question('Write your username: ', (questionData) => {
+      socket.write(questionData);
+      console.log('Type "exit" to quit.');
+    });
 
-  })
+    // Listening for type of client...
+    readline.on('line', (msgToSend)=>{
+        socket.write(msgToSend)
+        msgToSend === EXIT_SOCKET && socket.end()
+    })
+  });
+  socket.on('data', (data) => {
+    console.log(data);
+  });
   socket.on('error', (e) => error(e.message));
+  socket.on('close', () => {
+    console.log('Client disconnected');
+    process.exit(0);
+  });
 };
 const runClient = () => {
   if (process.argv.length !== 4) {
